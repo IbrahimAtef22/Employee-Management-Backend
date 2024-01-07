dca007371f48f8a5675ebbfc5d3521fac9e6c946
Ibrahim Atef Eltahlawy <82728944+IbrahimAtef22@users.noreply.github.com>
2024-01-07 18:30:42 +0200

    Delete src directory

--- src/main/java/com/jobhacker/springboot/employeeapp/exception/EmployeeAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.jobhacker.springboot.employeeapp.exception;
-
-public class EmployeeAlreadyExistsException extends RuntimeException{
-
-    public EmployeeAlreadyExistsException(String message) {
-        super(message);
-    }
-}
